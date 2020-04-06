0cf1e36a834b4968b701c7edc0b1d134cd4d8a63
Olga Bursina <bursina.olya@yandex.ru>
2020-04-06 15:59:35 +0500

    Added writers

---
+++ HuffmanCompression/Writers/ByteWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace HuffmanCompression.Writers
+{
+    public class ByteWriter
+    {
+        public void WriteToFile(string filePath, byte[] content)
+        {
+            FileStream stream;
+            try
+            {
+                stream = new FileStream(filePath, FileMode.Create);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Не удалось создать файл", e);
+            }
+
+            using (stream)
+            {
+                for (var i = 0; i < content.Length; i++)
+                {
+                    byte value = 0;
+                    for (byte Mask = 1; i < content.Length; Mask <<= 1, i++)
+                    {
+                        if (content[i] == 1)
+                            value |= Mask;
+                        if (Mask == 0x80)
+                            break;
+                    }
+                    
+                    stream.WriteByte(value);
+                }
+            }
+        }
+    }
+}
---
+++ HuffmanCompression/Writers/StringWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace HuffmanCompression.Writers
+{
+    public static class StringWriter
+    {
+        public static void WriteToFile(string filePath, string content)
+        {
+            FileStream stream;
+            try
+            {
+                stream = new FileStream(filePath, FileMode.Create);
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Не удалось создать файл", e);
+            }
+            File.WriteAllText (filePath, content);
+        }
+    }
+}
