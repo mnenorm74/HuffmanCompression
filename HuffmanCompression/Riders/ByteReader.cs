ffc70a58da136c6407204818bdf04a6034689d13
Olga Bursina <bursina.olya@yandex.ru>
2020-04-06 15:37:33 +0500

    Added readers

---
+++ HuffmanCompression/Riders/ByteReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace HuffmanCompression.Readers
+{
+    public class ByteReader
+    {
+        public BitArray ReadFile(string filePath)
+        {
+            try
+            {
+                return new BitArray(File.ReadAllBytes(filePath));
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Не удалось прочитать файл", e);
+            }
+        }
+    }
+}
---
+++ HuffmanCompression/Riders/TxtReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HuffmanCompression.Readers
+{
+    public class TxtReader
+    {
+        public string ReadFile(string filePath, Encoding encoding)
+        {
+            FileStream stream;
+            try
+            {
+                stream = new FileStream(filePath, FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Не удалось прочитать файл", e);
+            }
+            using (var streamReader = new StreamReader(stream, encoding))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
